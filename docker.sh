a1ec11d4d97bcb2e4b8c9bfce134fbc70d7315bb
nguyendangthaoit <nguyendangthaoit@gmail.com>
2024-10-18 23:21:35 +0800

    updaet docker.sh

--- docker.sh
+++ docker.sh
@@ -1,35 +1,11 @@
+# install-docker.sh
 #!/bin/bash
-
-# Update package information
 sudo apt-get update
-
-# Install packages to allow apt to use a repository over HTTPS
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common -y
-
-# Add Docker's official GPG key
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-# Set up the stable Docker repository
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-# Update the package database with Docker packages from the newly added repo
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-
-# Install Docker CE (Community Edition)
-sudo apt-get install docker-ce -y
-
-# Enable Docker service to start on boot
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker ${USER}
 sudo systemctl enable docker
-
-# Start Docker service
 sudo systemctl start docker
-
-# Verify Docker installation
-sudo docker --version
